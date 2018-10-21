62d435f0d1c29c454b6bb508c31970809e09e3b4
BuildTools <unconfigured@null.spigotmc.org>
2018-10-21 00:57:04 -0300

    Fix on main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 
 
 def main():
-     #ex = int(input("Type the exercise number as an integer (e.g.: 1): "))
-     ex = 3
+     ex = int(input("Type the exercise number as an integer (e.g.: 1): "))
+
      if (ex == 1):
           ex1()
      elif(ex == 2):
